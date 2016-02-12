5bbe55f00761c36c9c91c991abf6a843cfed3b88
luyor <807339657@qq.com>
2016-02-12 15:21:26 -0800

    add heart_animation

--- Assets/Scripts/collide.cs
+++ Assets/Scripts/collide.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class collide : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
-	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter2D(Collider2D other){
+		//gameObject.SetActive (false);
+		transform.Find("heart").gameObject.SetActive(true);
 	}
 }
