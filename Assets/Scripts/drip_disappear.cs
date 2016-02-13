015faa03601d39028b2fe841c368687c7bef1713
ruochen_j <jrc@d142-058-170-098.wireless.sfu.ca>
2016-02-12 20:45:06 -0800

    add level2

---
+++ Assets/Scripts/drip_disappear.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drip_disappear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Drip")) {
+			other.gameObject.SetActive(false);
+		}
+	}
+}
